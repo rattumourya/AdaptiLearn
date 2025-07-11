6644c6a08c7618e4cff1ef89b82567fb3683cb50
Ratan Lal Mourya <lalmouryaratan@gmail.com>
2025-07-11 19:09:40 +0000

    UI Bug : Side menu My Library Hidden due to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -43,7 +43,7 @@ export default function Header() {
   }
 
   return (
-    <header className="sticky top-0 z-30 flex h-16 items-center gap-4 border-b bg-background/80 px-4 backdrop-blur-sm md:px-6">
+    <header className="sticky top-0 z-40 flex h-16 items-center gap-4 border-b bg-background/80 px-4 backdrop-blur-sm md:px-6">
       <div className="flex items-center gap-4">
         <SidebarTrigger className="md:hidden"/>
         <Link
